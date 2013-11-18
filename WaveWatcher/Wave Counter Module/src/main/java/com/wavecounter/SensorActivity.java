4ac8605a8a3738508f372355fe765d2bee8eb269
rcompton <compton.ryan@gmail.com>
2013-11-17 20:48:05 -0800

    write accel data to disk

--- WaveWatcher/Wave Counter Module/src/main/java/com/wavecounter/SensorActivity.java
+++ WaveWatcher/Wave Counter Module/src/main/java/com/wavecounter/SensorActivity.java
@@ -6,25 +6,60 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.wavecounter.sensors.AccelerometerMeasurement;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ryan on 11/17/13.
  */
 public class SensorActivity extends Activity implements SensorEventListener {
     private static final String LOG_TAG = "wavecounter sensors";
+
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+    private Sensor mGyroscope;
+
+
+    private String fnameTimeStamp;
+
+    private List<AccelerometerMeasurement> accelerometerMeasurements;
+
+    /**
+     * rotating device calls oncreate?
+     */
+    public SensorActivity(){
+        fnameTimeStamp = Long.toString(System.currentTimeMillis());
+        accelerometerMeasurements = new ArrayList<AccelerometerMeasurement>();
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensors);
         Log.i(LOG_TAG, "sensor activity onCreate");
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        //sensor manager
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //register accelerometer
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //register gyroscope
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     protected void onResume() {
@@ -38,19 +73,49 @@ public class SensorActivity extends Activity implements SensorEventListener {
         mSensorManager.unregisterListener(this);
     }
 
+    /**
+     * user pressed save button, write all sensor data to disk and exit.
+     * @param view
+     */
+    public void saveButton(View view){
+
+        Log.i(LOG_TAG, Integer.toString(accelerometerMeasurements.size()) );
+        try{
+            File accelFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),
+                    "accel_data_"+fnameTimeStamp+".tsv");
+
+            Toast.makeText(getApplicationContext(), accelFile.toString(), Toast.LENGTH_LONG).show();
+
+            Log.i(LOG_TAG, accelFile.toString() );
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(accelFile));
+            for(AccelerometerMeasurement am : accelerometerMeasurements){
+                bw.write(am.toString() + "\n");
+            }
+            bw.close();
+
+        }catch (Exception e){
+            Log.e(LOG_TAG,"io problem", e);
+        }
+
+        finish();
+    }
+
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
-
     @Override
     public void onSensorChanged(SensorEvent event) {
+
+        long currentTime = System.currentTimeMillis();
 
         if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
             float ax,ay,az;
             ax=event.values[0];
             ay=event.values[1];
             az=event.values[2];
-            Log.i(LOG_TAG, ax + "," + ay + "," + az);
+            //Log.i(LOG_TAG, ax + "," + ay + "," + az);
+            accelerometerMeasurements.add(new AccelerometerMeasurement(currentTime, ax, ay, az));
         }
     }
 
